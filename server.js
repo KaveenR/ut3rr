e5a80e70ec3d0cfe3417fdff1aac6a3c00146133
Kaveen Rodrigo <u.k.k.rodrigo@gmail.com>
2015-10-20 11:51:46 +0530

    init

---
+++ server.js
@@ -0,0 +1,31 @@
+var exp = require('express');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var rooms = [];
+
+app.use('/', exp.static(__dirname + '/ui'));
+
+io.on('connection', function(socket){
+
+  socket.on('get_rooms', function(msg){
+    io.to(socket.id).emit("rooms",rooms.slice(rooms.length-50, rooms.length))
+  });
+
+  socket.on('join_room', function(msg){
+    socket.join(msg);
+    socket.room = msg
+    rooms.push(msg);
+    io.to(socket.id).emit("joined",msg)
+  });
+
+  socket.on('letter', function(msg,room){
+    socket.broadcast.to(room).emit("letter",socket.id,msg)
+  });
+
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
---
+++ ui/main.js
@@ -0,0 +1,57 @@
+var app = angular.module('StringGame', []);
+app.controller('mnCtrl', function($scope) {
+  $scope.in = false;
+  var socket = io();
+  $scope.room = {};
+  $scope.rooms = {};
+  $scope.room.rname = "";
+  $scope.room.users={};
+  $scope.letters = [];
+
+  socket.on('joined', function(msg){
+    $scope.room.rname = msg;
+    $scope.$apply();
+  });
+
+  $(function() {
+     $(window).keypress(function(e) {
+         if($scope.in){
+           var k = new String(String.fromCharCode(e.which));
+           k.ucolor="black";
+           //if (k.toUpperCase() == $scope.letters[$scope.letters.length - 1].toUpperCase()){
+             $scope.letters.push(k);
+             socket.emit("letter",k,$scope.room.rname)
+             $scope.$apply();
+           //}
+         }
+     });
+  });
+
+  socket.on('letter', function(sid,msg){
+    var t = new String(msg);
+    t.sid = sid;
+    if ($scope.room.users[sid] == undefined){
+      $scope.room.users[sid] = '#'+Math.random().toString(16).substr(-6);
+    }
+    t.ucolor = $scope.room.users[sid];
+    $scope.letters.push(t)
+    $scope.$apply();
+  });
+
+  socket.on('rooms', function(msg){
+    $scope.rooms = msg;
+    
+    $scope.$apply();
+  })
+
+  $scope.join = function(){
+    socket.emit('join_room', $scope.room.rname);
+    $scope.in = true;
+    $scope.$apply();
+  }
+
+  $scope.init = function(){
+    socket.emit('get_rooms', "");
+  }
+
+});
